fdc874f987b97f747ff10365f1073c66f8a7862b
Thomas Lack <thomas.lack@hypoport.de>
2020-03-19 19:21:56 +0100

    Add patienten module to app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,20 @@
-import { HttpClientModule } from '@angular/common/http';
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import {registerLocaleData} from "@angular/common";
+import {HttpClientModule} from "@angular/common/http";
+import localeDe from "@angular/common/locales/de";
+import {NgModule} from "@angular/core";
+import {FormsModule} from "@angular/forms";
+import {BrowserModule} from "@angular/platform-browser";
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
+import "reflect-metadata";
+import "../polyfills";
+import {AppRoutingModule} from "./app-routing.module";
+import {AppComponent} from "./app.component";
+import {CoreModule} from "./core/core.module";
+import {HomeModule} from "./home/home.module";
+import {PatientenModule} from "./patienten/patienten.module";
+import {SharedModule} from "./shared/shared.module";
 
-import { BrowserModule } from '@angular/platform-browser';
-import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
-import 'reflect-metadata';
-import '../polyfills';
-
-import { AppRoutingModule } from './app-routing.module';
-
-import { AppComponent } from './app.component';
-import { CoreModule } from './core/core.module';
-
-import { HomeModule } from './home/home.module';
-import { SharedModule } from './shared/shared.module';
+registerLocaleData(localeDe, "de-DE");
 
 @NgModule({
 	declarations: [AppComponent],
@@ -26,6 +27,7 @@ import { SharedModule } from './shared/shared.module';
 		SharedModule,
 		HomeModule,
 		AppRoutingModule,
+		PatientenModule,
 	],
 	providers: [],
 	bootstrap: [AppComponent],
