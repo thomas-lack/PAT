b2e4252b57746a10683ac6ae966d68f1bf5b5601
Thomas Lack <thomas.lack@hypoport.de>
2020-03-20 17:25:41 +0100

    Change selector app-root to pat-root

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,25 +1,24 @@
-import { Component } from '@angular/core';
-import { AppConfig } from '../environments/environment';
-import { ElectronService } from './core/services';
+import {Component} from "@angular/core";
+import {AppConfig} from "../environments/environment";
+import {ElectronService} from "./electron/electron.service";
 
 @Component({
-	selector: 'app-root',
-	templateUrl: './app.component.html',
-	styleUrls: ['./app.component.scss'],
+	selector: "pat-root",
+	templateUrl: "./app.component.html",
+	styleUrls: ["./app.component.scss"],
 })
 export class AppComponent {
-	constructor(
-		public electronService: ElectronService,
-	) {
-		console.log('AppConfig', AppConfig);
+	constructor(public electronService: ElectronService) {
+		console.log("AppConfig", AppConfig);
 
 		if (electronService.isElectron) {
 			console.log(process.env);
-			console.log('Mode electron');
-			console.log('Electron ipcRenderer', electronService.ipcRenderer);
-			console.log('NodeJS childProcess', electronService.childProcess);
-		} else {
-			console.log('Mode web');
+			console.log("Mode electron");
+			console.log("Electron ipcRenderer", electronService.ipcRenderer);
+			console.log("NodeJS childProcess", electronService.childProcess);
+		}
+		else {
+			console.log("Mode web");
 		}
 	}
 }
