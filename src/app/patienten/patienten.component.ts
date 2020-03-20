ead4852cfb8e25031c8232d801320df5e5c25aa5
Thomas Lack <thomas.lack@hypoport.de>
2020-03-20 17:28:34 +0100

    Apply codestyle to patienten component

--- src/app/patienten/patienten.component.ts
+++ src/app/patienten/patienten.component.ts
@@ -1,6 +1,7 @@
 import {Component, OnInit} from "@angular/core";
+import {Router} from "@angular/router";
 import {Observable} from "rxjs";
-import {Patient} from "./patient.interface";
+import {Patient} from "../patient/patient.interface";
 import {PatientenService} from "./patienten.service";
 
 @Component({
@@ -12,10 +13,18 @@ export class PatientenComponent implements OnInit {
 
 	public patienten: Observable<Patient[]>;
 
-	constructor(private patientenService: PatientenService) {
+	constructor(
+		private patientenService: PatientenService,
+		private router: Router,
+	) {
 	}
 
 	ngOnInit() {
 		this.patienten = this.patientenService.getPatienten();
+	}
+
+	onBackButtonClick() {
+		console.log("1");
+		this.router.navigate(["/"]);
 	}
 }
