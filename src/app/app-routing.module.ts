29de4bf0f897cd96759863e8e3e6110a1d740efc
Thomas Lack <thomas.lack@hypoport.de>
2020-03-23 18:04:49 +0100

    Add routing to patient/:id on Patienten table entry click

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -14,6 +14,10 @@ const routes: Routes = [
 		component: PatientComponent,
 	},
 	{
+		path: "patient/:id",
+		component: PatientComponent,
+	},
+	{
 		path: "patienten",
 		component: PatientenComponent,
 	},
